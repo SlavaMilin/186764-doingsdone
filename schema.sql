141c3d97d22cfb85f3db3377eabfc297cd253dd1
SlavaMilin <slava.milin@gmail.com>
2017-12-04 18:13:29 +0300

    исправления

--- schema.sql
+++ schema.sql
@@ -1,31 +1,31 @@
-CREATE DATABASE doingsdone;
+CREATE DATABASE doingsdone COLLATE utf8_general_ci;
 USE doingsdone;
 
-CREATE TABLE projects (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	project_name CHAR(128),
+CREATE TABLE IF NOT EXISTS projects (
+	project_id INT AUTO_INCREMENT PRIMARY KEY,
+	project_name CHAR(128)
+)
+  ENGINE = INNODB CHARACTER SET = utf8;
 
-	user_name CHAR(128)
-);
-CREATE TABLE tasks (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS tasks (
+	task_id INT AUTO_INCREMENT PRIMARY KEY,
 	task CHAR(128) NOT NULL,
-	date_start DATE,
-	date_finish DATE,
-	date_deadline DATE,
-	file_link CHAR(128),
+	date_start DATETIME,
+	date_finish DATETIME,
+	date_deadline DATETIME,
+	file_link CHAR(128)
+)
+  ENGINE = INNODB CHARACTER SET=utf8;
 
-	project_name CHAR(128),
-	user_name CHAR(128)
-);
-CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
 	user_name CHAR(128),
 	password CHAR(128) NOT NULL,
 	email CHAR(128) NOT NULL,
-	date_registration DATE,
+	date_registration DATETIME,
 	contacts_data CHAR(128)
-);
+)
+  ENGINE = INNODB CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX user_name ON users(user_name);
 CREATE UNIQUE INDEX email ON users(email);
