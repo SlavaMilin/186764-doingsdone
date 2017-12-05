3f3ebb3a8b65f051ca8ea25c6c169792bcac2dd9
SlavaMilin <slava.milin@gmail.com>
2017-12-05 16:43:51 +0300

    исправления

--- schema.sql
+++ schema.sql
@@ -3,29 +3,29 @@ USE doingsdone;
 
 CREATE TABLE IF NOT EXISTS projects (
 	project_id INT AUTO_INCREMENT PRIMARY KEY,
-	project_name CHAR(128)
-)
-  ENGINE = INNODB CHARACTER SET = utf8;
+	project_name VARCHAR(128),
+	user_id INT
+)ENGINE = INNODB CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS tasks (
 	task_id INT AUTO_INCREMENT PRIMARY KEY,
-	task CHAR(128) NOT NULL,
+	task VARCHAR(128) NOT NULL,
 	date_start DATETIME,
 	date_finish DATETIME,
 	date_deadline DATETIME,
-	file_link CHAR(128)
-)
-  ENGINE = INNODB CHARACTER SET=utf8;
+	file_link VARCHAR(128),
+	user_id INT,
+	project_id INT
+) ENGINE = INNODB CHARACTER SET=utf8;
 
 CREATE TABLE IF NOT EXISTS users (
 	user_id INT AUTO_INCREMENT PRIMARY KEY,
-	user_name CHAR(128),
+	user_name VARCHAR(128),
 	password CHAR(128) NOT NULL,
-	email CHAR(128) NOT NULL,
+	email VARCHAR(128) NOT NULL,
 	date_registration DATETIME,
-	contacts_data CHAR(128)
-)
-  ENGINE = INNODB CHARACTER SET = utf8;
+	contacts_data VARCHAR(128)
+) ENGINE = INNODB CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX user_name ON users(user_name);
 CREATE UNIQUE INDEX email ON users(email);
